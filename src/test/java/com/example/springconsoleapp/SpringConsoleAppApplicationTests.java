39e0eb8e6a10baae31bf7f99a12bf91d464f9d43
Maksym <maksym.koval@syncwords.com>
2022-09-07 18:37:02 +0000

    Initial commit

---
+++ src/test/java/com/example/springconsoleapp/SpringConsoleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springconsoleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsoleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
